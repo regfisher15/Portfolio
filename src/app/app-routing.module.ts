9acab39dd5aac69fb52bfda2972d36b1b3432bdc
rbfisher15 <rbfisher@crimson.ua.edu>
2024-05-21 11:30:21 -0500

    Finish skills page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { SkillsComponent } from './pages/skills/skills.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'skills', component: SkillsComponent },
 ];
 
 @NgModule ({
@@ -15,4 +17,4 @@ export const routes: Routes = [
 })
 
 export class AppRoutingModule {}
-export const routingComponents = [HomeComponent, AboutComponent]
+export const routingComponents = [HomeComponent, AboutComponent, SkillsComponent]
