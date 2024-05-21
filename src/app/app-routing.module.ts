c3119781797873eefcf42ec055b218ac7a2e0398
rbfisher15 <rbfisher@crimson.ua.edu>
2024-05-21 13:20:32 -0500

    Start on project page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SkillsComponent } from './pages/skills/skills.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'skills', component: SkillsComponent },
+    { path: 'projects', component: ProjectsComponent }
 ];
 
 @NgModule ({
@@ -17,4 +19,4 @@ export const routes: Routes = [
 })
 
 export class AppRoutingModule {}
-export const routingComponents = [HomeComponent, AboutComponent, SkillsComponent]
+export const routingComponents = [HomeComponent, AboutComponent, SkillsComponent, ProjectsComponent]
