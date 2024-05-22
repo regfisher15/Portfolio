c09f9b6c2a4696c97cba8281b22a61da3b3fd3ea
rbfisher15 <rbfisher@crimson.ua.edu>
2024-05-22 00:04:58 -0500

    Portfolio website complete for now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SkillsComponent } from './pages/skills/skills.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'skills', component: SkillsComponent },
-    { path: 'projects', component: ProjectsComponent }
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule ({
@@ -19,4 +21,4 @@ export const routes: Routes = [
 })
 
 export class AppRoutingModule {}
-export const routingComponents = [HomeComponent, AboutComponent, SkillsComponent, ProjectsComponent]
+export const routingComponents = [HomeComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent]
